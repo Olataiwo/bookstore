b4e43d358072ec2e0c79cc8be2c34da4914cfb97
dimeji <dimeji.taiwo5@outlook.com>
2017-04-26 03:05:12 +0100

    header and footer sliced

---
+++ www/includes/footer.php
@@ -0,0 +1,9 @@
+
+<?php $today = date('Y');?>
+<section class="foot">
+		<div>
+			<p>&copy; <?php echo $today; ?>
+		</div>
+	</section>
+</body>
+</html>
---
+++ www/includes/header.php
@@ -0,0 +1,14 @@
+
+<html>
+<head>
+	<title><?php $title ?></title>
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+</head>
+<body>
+	<section>
+		<div class="mast">
+			<h1>T<span>SSB</span></h1>
+		</div>
+	</section>
+	
+	<div class="wrapper">
