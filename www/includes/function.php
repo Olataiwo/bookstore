c21bad5377ad4ec39255cfc0a67f7eddb1b0804c
dimeji <dimeji.taiwo5@outlook.com>
2017-04-26 22:06:39 +0100

    register in db

--- www/includes/function.php
+++ www/includes/function.php
@@ -10,6 +10,27 @@ class Utils {
 			}
 		} 
 
+	public static function registerAdmin($dbconn,$input) {
+
+			$stmt = $dbconn->prepare("INSERT into admin (firstname,lastname,email,hash) VALUES(:f,:l,:e,:h)");
+
+			$data = [
+
+				":f" => $input['fname'],
+
+				":l" => $input['lname'],
+
+				":e" => $input['email'],
+
+				":h" => $input['password'],
+
+
+			];
+
+			$stmt->execute($data);
+
+	}
+
 }
 
 ?>
